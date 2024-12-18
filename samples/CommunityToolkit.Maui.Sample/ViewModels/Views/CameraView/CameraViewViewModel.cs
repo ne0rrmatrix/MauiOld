a8eb097f0dfb2f32706fe62e1b8a18a55c634650
James Crutchley <ne0rmatrix@gmail.com>
2024-12-18 00:12:08 -0800

    Fix Observable Properties

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Views/CameraView/CameraViewViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Views/CameraView/CameraViewViewModel.cs
@@ -7,61 +7,69 @@ namespace CommunityToolkit.Maui.Sample.ViewModels.Views;
 
 public partial class CameraViewViewModel(ICameraProvider cameraProvider) : BaseViewModel
 {
-	[ObservableProperty]
-	public partial CameraFlashMode FlashMode { get; set; }
+    [ObservableProperty]
+    public partial CameraFlashMode FlashMode { get; set; }
+
+    [ObservableProperty]
+    public partial CameraInfo? SelectedCamera { get; set; }
+
+    [ObservableProperty]
+    public partial Size SelectedResolution { get; set; }
+
+    [ObservableProperty]
+    public partial float CurrentZoom { get; set; }
 
 	[ObservableProperty]
-	public partial CameraInfo? SelectedCamera { get; set; }
-
+	public partial string CameraNameText { get; set; } = string.Empty;
+    [ObservableProperty]
+    public partial string ZoomRangeText { get; set; } = string.Empty;
 	[ObservableProperty]
-	public partial Size SelectedResolution { get; set; }
-
+    public partial string CurrentZoomText { get; set; } = string.Empty;
 	[ObservableProperty]
-	public partial float CurrentZoom { get; set; }
-
+    public partial string FlashModeText { get; set; } = string.Empty;
 	[ObservableProperty]
-	string cameraNameText = "", zoomRangeText = "", currentZoomText = "", flashModeText = "", resolutionText = "";
+    public partial string ResolutionText { get; set; } = string.Empty;
 
 	public IReadOnlyList<CameraInfo> Cameras => cameraProvider?.AvailableCameras ?? [];
 
-	public CancellationToken Token => CancellationToken.None;
+    public CancellationToken Token => CancellationToken.None;
 
-	public ICollection<CameraFlashMode> FlashModes { get; } = Enum.GetValues<CameraFlashMode>();
+    public ICollection<CameraFlashMode> FlashModes { get; } = Enum.GetValues<CameraFlashMode>();
 
-	[RelayCommand]
-	async Task RefreshCameras(CancellationToken token) => await cameraProvider.RefreshAvailableCameras(token);
+    [RelayCommand]
+    async Task RefreshCameras(CancellationToken token) => await cameraProvider.RefreshAvailableCameras(token);
 
-	partial void OnFlashModeChanged(CameraFlashMode value)
-	{
-		UpdateFlashModeText();
-	}
+    partial void OnFlashModeChanged(CameraFlashMode value)
+    {
+        UpdateFlashModeText();
+    }
 
-	partial void OnCurrentZoomChanged(float value)
-	{
-		UpdateCurrentZoomText();
-	}
+    partial void OnCurrentZoomChanged(float value)
+    {
+        UpdateCurrentZoomText();
+    }
 
-	partial void OnSelectedResolutionChanged(Size value)
-	{
-		UpdateResolutionText();
-	}
+    partial void OnSelectedResolutionChanged(Size value)
+    {
+        UpdateResolutionText();
+    }
 
-	void UpdateFlashModeText()
-	{
-		if (SelectedCamera is null)
-		{
-			return;
-		}
-		FlashModeText = $"{(SelectedCamera.IsFlashSupported ? $"Flash mode: {FlashMode}" : "Flash not supported")}";
-	}
+    void UpdateFlashModeText()
+    {
+        if (SelectedCamera is null)
+        {
+            return;
+        }
+        FlashModeText = $"{(SelectedCamera.IsFlashSupported ? $"Flash mode: {FlashMode}" : "Flash not supported")}";
+    }
 
-	void UpdateCurrentZoomText()
-	{
-		CurrentZoomText = $"Current Zoom: {CurrentZoom}";
-	}
+    void UpdateCurrentZoomText()
+    {
+        CurrentZoomText = $"Current Zoom: {CurrentZoom}";
+    }
 
-	void UpdateResolutionText()
-	{
-		ResolutionText = $"Selected Resolution: {SelectedResolution.Width} x {SelectedResolution.Height}";
-	}
+    void UpdateResolutionText()
+    {
+        ResolutionText = $"Selected Resolution: {SelectedResolution.Width} x {SelectedResolution.Height}";
+    }
 }
